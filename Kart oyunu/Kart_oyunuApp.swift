38a31b53d2c4184ddebb5617f41fffcdc0cfb728
Muhammet <134100718+Muhammetsaman@users.noreply.github.com>
2023-07-31 05:14:12 +0300

    Initial Commit

---
+++ Kart oyunu/Kart_oyunuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kart_oyunuApp.swift
+//  Kart oyunu
+//
+//  Created by Muhammet on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Kart_oyunuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
